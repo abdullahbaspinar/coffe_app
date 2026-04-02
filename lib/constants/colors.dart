5f2e5719497c5b8566a3e950332767775c00c368
Abdullah Baspinar <aabdullahbaspinarr@gmail.com>
2026-04-02 17:27:07 +0300

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+class Colors {
+  static const Color primaryColor = Color(0xFF00a336); //koyu yeşil
+  static const Color secondaryColor = Color(0xFFededed);
+}
