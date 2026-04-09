b833f6e5d595e4bf23b03f441f39b260cff81add
Abdullah Baspinar <aabdullahbaspinarr@gmail.com>
2026-04-09 11:29:30 +0300

    home page product card add

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -9,7 +9,48 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class Product {
+  final String imagePath;
+  final String title;
+  final String price;
+  final String oldPrice;
+
+  Product({
+    required this.imagePath,
+    required this.title,
+    required this.price,
+    required this.oldPrice,
+  });
+}
+
 class _HomePageState extends State<HomePage> {
+  final List<Product> products = [
+    Product(
+      imagePath: "assets/product/product1.png",
+      title: "Ice Latte",
+      price: "\$5.8",
+      oldPrice: "\$9.9",
+    ),
+    Product(
+      imagePath: "assets/product/product2.png",
+      title: "Caramel Latte",
+      price: "\$6.2",
+      oldPrice: "\$8.5",
+    ),
+    Product(
+      imagePath: "assets/product/product1.png",
+      title: "Mocha Frappe",
+      price: "\$7.1",
+      oldPrice: "\$10.0",
+    ),
+    Product(
+      imagePath: "assets/product/product2.png",
+      title: "Mocha Frappe",
+      price: "\$7.1",
+      oldPrice: "\$10.0",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,25 +60,35 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  get _buildAppBar {
-    return AppBar(title: Text("Anasayfa"));
+  AppBar get _buildAppBar {
+    return AppBar(
+      title: const Text("Anasayfa"),
+    );
   }
 
   Widget get _buildBody {
     return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          ProductCard(
-            imagePath: "assets/product/product1.png",
-            title: "Creamy İce Coffe",
-            price: "\$5.8",
-            oldPrice: "\$9.9",
-            onTap: () {
-              print("product card tıklandı");
-            },
-          ),
-        ],
+      padding: const EdgeInsets.all(16),
+      child: SizedBox(
+        height: 270,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            final product = products[index];
+
+            return Padding(
+              padding: const EdgeInsets.only(right: 12),
+              child: ProductCard(
+                imagePath: product.imagePath,
+                title: product.title,
+                price: product.price,
+                oldPrice: product.oldPrice,
+                onTap: () {},
+              ),
+            );
+          },
+        ),
       ),
     );
   }
