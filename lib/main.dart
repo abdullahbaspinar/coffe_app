90572e042e8fac68d573fd774d5609e166191dd0
Abdullah Başpınar <50599388+abdullahbaspinar@users.noreply.github.com>
2026-04-15 16:42:23 +0300

    Revert "product detail sayfası yapıldı"

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'package:coffe_app/view/auth/sign_in_page.dart';
 import 'package:coffe_app/view/auth/sign_up_page.dart';
 import 'package:coffe_app/view/home/home_page.dart';
 import 'package:coffe_app/view/onboarding/onboarding_page.dart';
-import 'package:coffe_app/view/product/product_detail.dart';
 import 'package:coffe_app/view/splash/splash_screen.dart';
-import 'package:coffe_app/view/splash/splash_screen.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,11 +16,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const ProductDetail(),
+      theme: ThemeData(
+        
+   
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SplashScreen(),
     );
   }
 }
+
