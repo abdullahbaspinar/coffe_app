86b4c4f9fa0e591133976249be686062b247e64b
Abdullah Baspinar <aabdullahbaspinarr@gmail.com>
2026-06-29 16:35:33 +0300

    fix:home page mixin eklendi

---
+++ lib/model/home-product.dart
@@ -0,0 +1,13 @@
+class HomeProduct {
+  final String imagePath;
+  final String title;
+  final String price;
+  final String oldPrice;
+
+  HomeProduct({
+    required this.imagePath,
+    required this.title,
+    required this.price,
+    required this.oldPrice,
+  });
+}
